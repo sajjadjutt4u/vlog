816f1f9f0878606e5cb1ea6080bdd1034764f7db
Sajjad Ali <bcsf16a521@pucit.edu.pk>
2020-06-01 11:06:01 +0500

    add homeControler

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@
     .module("myApp", ["ui.router"])
 
     .config(RoutesConfig)
-    .controller("loginController", function () {});
+    .controller("loginController", function () {})
+    .controller("homeController", function ($scope) {
+      console.log($scope.username);
+    });
 
   RoutesConfig.$inject = ["$stateProvider", "$urlRouterProvider"];
   function RoutesConfig($stateProvider, $urlRouterProvider) {
@@ -12,6 +15,14 @@
     $stateProvider.state("login", {
       url: "/login",
       templateUrl: "./src/login.html",
+    });
+
+    $stateProvider.state("homePage", {
+      url: "/homePage?username",
+      templateUrl: "./src/home.html",
+      controller: function ($scope, $stateParams) {
+        $scope.username = $stateParams.username;
+      },
     });
   }
 })();
