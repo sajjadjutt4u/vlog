04d451f525708352452bebf3e3385f7f4fd1f9d5
Sajjad Ali <bcsf16a521@pucit.edu.pk>
2020-06-01 13:08:50 +0500

    session for users implementation

--- src/app.js
+++ src/app.js
@@ -4,17 +4,32 @@
 
     .config(RoutesConfig)
     .controller("loginController", function () {})
-    .controller("homeController", function ($scope) {
-      console.log($scope.username);
+    .controller("notRegisteredController", function () {})
+    .controller("homeController", function ($scope, $window, apicall) {
+      if (sessionStorage.getItem("username") != null) {
+        console.log("Session Data:", sessionStorage.getItem("username"));
+      } else {
+        $window.location.href = "#!/login";
+      }
+    })
+    .factory("apicall", function ($http) {
+      var service = this;
+      // service.getHomeVideos = function (username) {
+      //   $http.get();
+      // };
+      return service;
     });
 
   RoutesConfig.$inject = ["$stateProvider", "$urlRouterProvider"];
   function RoutesConfig($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/login");
-
     $stateProvider.state("login", {
       url: "/login",
       templateUrl: "./src/login.html",
+    });
+
+    $stateProvider.state("notRegistered", {
+      url: "/notRegistered",
+      templateUrl: "./src/notRegistered.html",
     });
 
     $stateProvider.state("home", {
@@ -22,7 +37,10 @@
       templateUrl: "./src/home.html",
       controller: function ($scope, $stateParams) {
         $scope.username = $stateParams.username;
+        sessionStorage.setItem("username", $stateParams.username);
       },
     });
+
+    $urlRouterProvider.otherwise("/login");
   }
 })();
